6314734bb87906dd0a55a4abb1b2df09da95a20f
csinga <oszlanyi.csenge624@gmail.com>
2023-07-19 23:11:00 +0200

    delete site and add contact page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SiteComponent } from './site/site.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
-  { path: 'site', component: SiteComponent },
+  { path: '', component: HomeComponent },
+  { path: 'kapcsolat', component: ContactComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
 
--- src/app/site/site.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'site',
-  templateUrl: './site.component.html',
-  styleUrls: ['./site.component.css']
-})
-export class SiteComponent {}
