37de366a7e655de043f64561d167702644a63f97
Csenge Oszlányi-Salacz <47918101+csinga24@users.noreply.github.com>
2023-07-19 23:21:29 +0200

    Add init content (#5)
    * add angular material
    
    * delete site and add contact page
    
    * delete unnecessary code
    
    * add toolbar and image to the hame page
    
    * optimized image

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from  '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 import { Router } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { SiteComponent } from './site/site.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SiteComponent, 
     HomeComponent, 
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
